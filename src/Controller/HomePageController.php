3a97bf91d0da9b15331a80b45521ad0ad3afe639
SabrinaBenoudiba <sabrina.benoudiba@gmail.com>
2025-09-08 16:08:07 +0200

    update homePagecontroller, twig..

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,22 +2,20 @@
 
 namespace App\Controller;
 
-use App\Repository\CategoryRepository;
 use App\Repository\RecipeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomePageController extends AbstractController
 {
     #[Route('/', name: 'app_home_page', methods: ['GET'])]
-    public function index(RecipeRepository $recipeRepository, CategoryRepository $categoryRepository, Request $request): Response
+    public function index(RecipeRepository $recipeRepository): Response
     {
-        $recipeRepository->findby([],['id'=>"DESC"]);
+
         return $this->render('home_page/index.html.twig', [
             'controller_name' => 'HomePageController',
-            'categories' => $categoryRepository -> findAll()
+            'recipes' => $recipeRepository->findAll(),
         ]);
     }
 }
